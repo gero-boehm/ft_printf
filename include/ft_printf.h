81d50de5b7c4c407d3d123545fa0b4bb92d8f7d4
Gero Bohm <gbohm@1-B-25.42heilbronn.de>
2022-12-01 10:05:18 +0100

    2022-12-01T10:05:09 initial refactoring

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: gbohm <gbohm@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 12:31:09 by gbohm             #+#    #+#             */
-/*   Updated: 2022/11/29 09:54:00 by gbohm            ###   ########.fr       */
+/*   Updated: 2022/11/30 13:58:40 by gbohm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define FT_PRINTF_H
 
 # include <stddef.h>
+# include <stdarg.h>
 
 // typedef int	t_bool;
 // # define true 1;
@@ -74,10 +75,13 @@ int		apply_prefix(t_tag *tag);
 int		init_buffer(const char *format, t_buffer *buffer);
 void	init_tag(t_tag *tag);
 int		parse_tag(t_buffer *buffer, t_tag *tag);
+int		evaluate(va_list args, t_tag *tag);
 
 int		is_char_specifier(t_tag *tag);
 int		is_str_specifier(t_tag *tag);
 int		is_ptr_specifier(t_tag *tag);
+int		is_hex_lower_specifier(t_tag *tag);
+int		is_hex_upper_specifier(t_tag *tag);
 int		is_hex_specifier(t_tag *tag);
 int		is_int_specifier(t_tag *tag);
 int		is_unsigned_specifier(t_tag *tag);
@@ -95,30 +99,26 @@ int		substr2(unsigned int start, size_t length, char **str);
 int		padstr2(int padding, int right, int zeroes, t_result *result);
 // int		padstr2(int padding, int right, int zeroes, char **str);
 int		malloc2(size_t count, char **str);
-int		itoa_base2(unsigned long long value, char *base, char **str);
+int		itoa_base2(unsigned long value, char *base, char **str);
 void	set_result_str(char *str, t_tag *tag);
 
 int		get_str_for_prefix2(t_prefix prefix, char **str);
 int		get_prefix_length(t_prefix prefix);
 
-int		prepare_str(t_tag *tag);
+void	init_tag(t_tag *tag);
+void	free_tag(t_tag *tag);
+int		has_left_justify_flag(t_tag *tag);
+int		has_plus_flag(t_tag *tag);
+int		has_space_flag(t_tag *tag);
+int		has_prefix_flag(t_tag *tag);
+int		has_zeroes_flag(t_tag *tag);
+int		has_padding(t_tag *tag);
+int		has_precision(t_tag *tag);
 
-// char	*ft_eval_char(t_tag *tag, int value);
-// char	*ft_eval_str(t_tag *tag, char *value);
-// char	*ft_eval_hex(t_tag *tag, unsigned long value);
-// char	*ft_eval_int(t_tag *tag, int value);
-// char	*ft_eval_unsigned(t_tag *tag, unsigned int value);
-// char	*zero_str(char *value, t_tag *tag);
-// char	*pad_str(char *value, t_tag *tag);
-// char	*build_str(char *value, char *prefix, t_tag *tag);
-// char	*eval_num(unsigned long long value, char *base);
+void	modify_buffer_size_and_cursor(t_tag *tag, t_buffer *buffer);
+void	free_buffer(t_buffer *buffer);
 
-// int		parse_tag(char *str, t_tag *tag, int *length);
-
-// int		strsub(char **str, unsigned int position, size_t length, char *sub);
-
-// // remove later
-// void	initialize_tag(t_tag *tag);
-// int		is_flag(char c);
+int		min(int a, int b);
+int		max(int a, int b);
 
 #endif
